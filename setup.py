63964f451b435ed37029bf98430a4d900ab5efe2
Nathan Hui <ntlhui@gmail.com>
2023-02-10 14:53:39 -0800

    Initial changes

--- setup.py
+++ setup.py
@@ -1,17 +1,34 @@
-"""Example setup file
+"""E4E Data Management Tools
 """
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
+from e4e_data_management import __version__
 
 setup(
-    name='example_package',
-    version='0.0.0.1',
+    name='e4e_data_management',
+    version=__version__,
     author='UCSD Engineers for Exploration',
     author_email='e4e@eng.ucsd.edu',
     entry_points={
         'console_scripts': [
-            'ExamplePythonConsoleScript = example_package.example_module:exampleEntryPoint'
+            'E4EDuplicator = e4e_data_management.duplicator:gui',
+            'E4EDataWrangler = e4e_data_management.data_wrangler:gui',
+            'E4ECommitter = e4e_data_management.storage:gui',
+            'E4EArchiver = e4e_data_management.archive:gui',
+            'E4EDuplicatorCli = e4e_data_management.duplicator:cli',
+            'E4EDataWranglerCli = e4e_data_management.data_wrangler:cli',
+            'E4ECommitterCli = e4e_data_management.storage:cli',
+            'E4EArchiverCli = e4e_data_management.archive:cli'
         ]
     },
     packages=find_packages(),
-    install_requires=[]
+    install_requires=[],
+    extras_require={
+        'dev': [
+            'pytest',
+            'coverage',
+            'pylint',
+            'wheel',
+        ]
+    },
 )
