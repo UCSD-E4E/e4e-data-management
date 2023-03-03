af13f5f54a301d63ef5ddb8f22c54235c17800ac
Nathan Hui <nthui@eng.ucsd.edu>
2023-03-02 18:24:50 -0800

    Updated to use fixture

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,6 +4,7 @@ import datetime as dt
 from pathlib import Path
 from shlex import split
 from tempfile import TemporaryDirectory
+from typing import Tuple
 from unittest.mock import Mock, patch
 
 from e4e_data_management.cli import main
@@ -27,33 +28,32 @@ def test_init_dataset():
             directory=Path('.')
         )
 
-def test_init_mission():
+def test_init_mission(test_mock_app: Tuple[Mock, DataManager, Path]):
     """Tests init_mission
     """
-    with TemporaryDirectory() as temp_dir:
-        DataManager.config_dir = Path(temp_dir)
+    mock, app, root_dir = test_mock_app
 
-        e4edm_mock = Mock(spec=DataManager(app_config_dir=Path(temp_dir)))
-        e4edm_mock.load.return_value = e4edm_mock
+    app.initialize_dataset(
+        date=dt.date(2023,3,2),
+        project='TEST',
+        location='San Diego',
+        directory=root_dir
+    )
 
-        args = split('e4edm init_dataset --date 2023-03-02 --project "TEST" --location "San Diego" '
-                     f'--path {temp_dir}')
-        with patch('sys.argv', args):
-            main()
-        args = split('e4edm init_mission --timestamp 2023-03-02T15:06-08:00 --device Device1 '
-                    '--country USA --region California --site SD --name RUN001')
-        with patch('sys.argv', args),\
-                patch('e4e_data_management.cli.DataManager', e4edm_mock):
-            main()
-            e4edm_mock.initialize_mission.assert_called_once_with(
-                metadata=Metadata(
-                    timestamp=dt.datetime(2023, 3, 2, 15, 6,
-                                          tzinfo=dt.timezone(dt.timedelta(hours=-8))),
-                    country='USA',
-                    device='Device1',
-                    region='California',
-                    site='SD',
-                    mission='RUN001',
-                    notes=None
-                )
+    args = split('e4edm init_mission --timestamp 2023-03-02T15:06-08:00 --device Device1 '
+                '--country USA --region California --site SD --name RUN001')
+    with patch('sys.argv', args),\
+            patch('e4e_data_management.cli.DataManager', mock):
+        main()
+        mock.initialize_mission.assert_called_once_with(
+            metadata=Metadata(
+                timestamp=dt.datetime(2023, 3, 2, 15, 6,
+                                        tzinfo=dt.timezone(dt.timedelta(hours=-8))),
+                country='USA',
+                device='Device1',
+                region='California',
+                site='SD',
+                mission='RUN001',
+                notes=None
             )
+        )
